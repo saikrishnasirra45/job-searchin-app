c45b1f9d7edc80eba8f5cd099b880882124c76fc
saikrishnasirra45 <saikrishnasirra@gmail.com>
2024-02-28 09:36:47 +0530

    feature signup working

---
+++ src/components/FiltersGroup/index.js
@@ -0,0 +1,123 @@
+import {BsSearch} from 'react-icons/bs'
+// eslint-disable-next-line import/extensions
+import ProfileDetails from '../ProfileDetails'
+import './index.css'
+
+const FiltersGroup = props => {
+  const onChangeSearchInput = event => {
+    const {changeSearchInput} = props
+    changeSearchInput(event)
+  }
+  const onEnterSearchInput = event => {
+    const {getJobs} = props
+    if (event.key === 'Enter') {
+      getJobs()
+    }
+  }
+  const renderSearchInput = () => {
+    const {getJobs, searchInput} = props
+    return (
+      <div className="search-input-container">
+        <input
+          type="search"
+          className="search-input"
+          placeholder='"Search'
+          value={searchInput}
+          onChange={onChangeSearchInput}
+          onKeyDown={onEnterSearchInput}
+        />
+        <button
+          type="button"
+          id="searchButton"
+          className="search-button-container"
+          onClick={getJobs}
+        >
+          <BsSearch className="search-icon" />
+        </button>
+      </div>
+    )
+  }
+  const renderTypeOfEmployment = () => {
+    const {employmentTypeList} = props
+    return (
+      <div className="employment-type-container">
+        <h1 className="employment-type-heading">Type of Employment</h1>
+        <u className="employee-type-list-container">
+          {employmentTypeList.map(eachEmployeeType => {
+            const {changeEmployeeList} = props
+            const onSelectEmployeeType = event => {
+              changeEmployeeList(event.target.value)
+            }
+            return (
+              <li
+                className="employee-item"
+                key={eachEmployeeType.employmentTypeId}
+                onChange={onSelectEmployeeType}
+              >
+                <input
+                  type="checkbox"
+                  id={eachEmployeeType.employmentTypeId}
+                  className="checkbox-input"
+                  value={eachEmployeeType.employmentTypeId}
+                />
+                <label
+                  htmlFor={eachEmployeeType.employmentTypeId}
+                  className="check-label"
+                >
+                  {eachEmployeeType.label}
+                </label>
+              </li>
+            )
+          })}
+        </u>
+      </div>
+    )
+  }
+  const renderSalaryRange = () => {
+    const {salaryRangeList} = props
+    return (
+      <div className="salary-range-container">
+        <h1 className="salary-range-headingg">Salary Range</h1>
+        <ul className="salary-range-list-container">
+          {salaryRangeList.map(eachSalary => {
+            const {changeSalary} = props
+            const onClickSalary = () => {
+              changeSalary(eachSalary.salaryRangeId)
+            }
+            return (
+              <li
+                className="salary-item"
+                key={eachSalary.salaryRangeId}
+                onClick={onClickSalary}
+              >
+                <input
+                  type="radio"
+                  id={eachSalary.salaryRangeId}
+                  name="salary"
+                  className="check-input"
+                />
+                <label
+                  htmlFor={eachSalary.salaryRangeId}
+                  className="check-label"
+                >
+                  {eachSalary.label}
+                </label>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    )
+  }
+  return (
+    <div className="filters-group-container">
+      {renderSearchInput()}
+      <ProfileDetails />
+      <hr className="horizantal-line" />
+      {renderTypeOfEmployment()}
+      <hr className="horizantal-line" />
+      {renderSalaryRange()}
+    </div>
+  )
+}
+export default FiltersGroup
